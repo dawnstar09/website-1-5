a0128e2612b49e8a991f208f303d768fa4732dc2
dawnstar09 <dawnstar09@naver.com>
2025-08-12 01:58:59 +0900

    fix: use PORT environment variable for cloudtype deployment

--- 1-5 website/my-website/flask_app.py
+++ 1-5 website/my-website/flask_app.py
@@ -521,4 +521,5 @@ def delete_suhang(suhang_id):
         })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
